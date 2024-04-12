9faa681fa1a2b9d2e91acfbcce35aa981c9bab3c
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2024-04-12 22:36:04 +0200

    Syntax highlighting works for class guards and attribute lists
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- languages/cfengine/highlights.scm
+++ languages/cfengine/highlights.scm
@@ -3,8 +3,25 @@
 ;; Bundles:
 (bundle_keyword) @keyword
 (bundle_type) @keyword
-;; (bundle_name) @variable
-;; (promise_guard) @type
-;; (promiser) @string
-;; (attribute_name) @keyword
-;; (quoted_string) @string
+(bundle_name) @variable
+(promise_guard) @type
+(promiser) @string
+(attribute_name) @parameter
+(quoted_string) @string
+(class_guard) @attribute
+["=>" "->"] @operator
+
+[
+  ";"
+  ","
+] @punctuation.delimiter
+
+[
+  "("
+  ")"
+] @punctuation.bracket
+
+[
+  "{"
+  "}"
+] @punctuation.bracket
