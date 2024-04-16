26883443f92642bd63317c0991efc4f08fc21ab5
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2024-04-16 17:14:55 +0200

    Prepared extension for first release
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# zed-cfengine
+# CFEngine extension for Zed editor
+
+This extension adds syntax highlighting for CFEngine policy language files (`.cf`) to the Zed editor.
+In the future it could be updated to add indentation / code formatting, linting, and autocomplete suggestions.
+
+It uses the tree-sitter grammar from this repo:
+
+https://github.com/olehermanse/tree-sitter-cfengine
--- languages/cfengine/highlights.scm
+++ languages/cfengine/highlights.scm
@@ -1,27 +1,34 @@
 (comment) @comment
 
-;; Bundles:
-(bundle_keyword) @keyword
-(bundle_type) @keyword
-(bundle_name) @variable
+(macro) @keyword
+(bundle_block_keyword) @keyword
+(bundle_block_type) @keyword
+(bundle_block_name) @variable
+(body_block_keyword) @keyword
+(body_block_type) @keyword
+(body_block_name) @variable
+(promise_block_keyword) @keyword
+(promise_block_type) @keyword
+(promise_block_name) @variable
+
 (promise_guard) @type
-(promiser) @string
-(attribute_name) @parameter
-(quoted_string) @string
 (class_guard) @attribute
+(promiser) @string
+(attribute_name) @variable
+
+(quoted_string) @string
+(qualified_identifier) @property
+(calling_identifier) @attribute
+
+(dollar_expression) @operator
+(at_expression) @operator
+(dollar_expression_identifier) @property
+(at_expression_identifier) @property
+
+[";" ","] @punctuation.delimiter
+
+["(" ")"] @punctuation.bracket
+
+["{" "}"] @punctuation.bracket
+
 ["=>" "->"] @operator
-
-[
-  ";"
-  ","
-] @punctuation.delimiter
-
-[
-  "("
-  ")"
-] @punctuation.bracket
-
-[
-  "{"
-  "}"
-] @punctuation.bracket
